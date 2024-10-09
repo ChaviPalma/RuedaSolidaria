fbdac7821f0014d0257c54cc2db150e04ae607ff
Bárbara <ba.palmam@duocuc.cl>
2024-10-09 01:39:12 -0300

    Muestra datos

---
+++ RuedaSolidaria/app.py
@@ -0,0 +1,24 @@
+# app.py
+
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configuración de la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'RuedaSolidaria'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Alumnos")  # Consulta para obtener todos los alumnos
+    alumnos = cursor.fetchall()  # Recupera todos los resultados
+    cursor.close()  # Cierra la conexión del cursor
+    return render_template('index.html', alumnos=alumnos)
+if __name__ == '__main__':
+    app.run(debug=True)
